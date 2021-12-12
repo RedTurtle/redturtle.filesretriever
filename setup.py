43d0b7e8956ee6d0e14850e7839780ade7390417
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-12-12 17:28:36 +0100

    fix dependencies

--- setup.py
+++ setup.py
@@ -48,14 +48,11 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "setuptools",
-        # -*- Extra requirements: -*-
-        "z3c.jbot",
         "plone.api>=1.8.4",
-        "plone.restapi < 8.0.0",
-        "plone.app.dexterity",
+        "plone.restapi",
     ],
     extras_require={
         "test": [
